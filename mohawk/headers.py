4a4683aaf4884ead3648ce61b11b9161b80b0925
Adrian Perez de Castro <aperez@igalia.com>
2014-12-07 01:15:57 +0100

    Add mohawk.headers module

---
+++ mohawk/headers.py
@@ -0,0 +1,183 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Adrian Perez <aperez@igalia.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Utilities to work with HTTP headers.
+"""
+
+from six import string_types
+from functools import total_ordering
+
+
+@total_ordering
+class Header(object):
+    # Name of the header, e.g. Content-Type
+    name = None
+    single_value = False
+
+    __slots__ = ("_value",)
+
+    def __init__(self, value):
+        self._value = value
+
+    def __repr__(self):  # pragma: no cover
+        return "<Header:{} {!r}>".format(self.name, self.string_value)
+
+    def __str__(self):
+        return "{}: {}\r\n".format(self.name, self.string_value)
+
+    @property
+    def string_value(self):
+        return str(self._value)
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ \
+           and self._value == other._value
+
+    def __lt__(self, other):
+        assert isinstance(other, Header)
+        return self.name < other.name \
+            or self._value < other._value
+
+
+class _StringHeader(Header):
+    pass
+
+
+class CustomHeader(_StringHeader):
+    __slots__ = ("name",)
+
+    def __init__(self, name, value):
+        super(CustomHeader, self).__init__(value)
+        self.name = str(name)
+
+
+class _NumericHeader(Header):
+    def __init__(self, value):
+        super(_NumericHeader, self).__init__(int(value))
+
+
+class _EnumHeader(Header):
+    values = ()
+
+    def __init__(self, value=0):
+        if isinstance(value, string_types):
+            value = self.values.index(value)
+        assert value < len(self.values)
+        super(_EnumHeader, self).__init__(value)
+
+    @property
+    def string_value(self):
+        return self.values[self._value]
+
+
+class ContentType(_StringHeader):
+    name = "Content-Type"
+    single_value = True
+
+
+class ContentLength(_NumericHeader):
+    name = "Content-Length"
+    single_value = True
+
+
+class Connection(_EnumHeader):
+    name = "Connection"
+    values = ("close", "keep-alive")
+    single_value = True
+
+Connection.CLOSE, \
+Connection.KEEP_ALIVE \
+    = map(Connection, range(len(Connection.values)))
+
+
+class Host(_StringHeader):
+    name = "Host"
+
+    def __init__(self, value, port=None):
+        value = str(value)
+        if port is not None:
+            value = "{}:{}".format(value, int(port))
+        super(Host, self).__init__(value)
+
+
+class UserAgent(_StringHeader):
+    name = "User-Agent"
+
+
+class Server(_StringHeader):
+    name = "Server"
+
+
+class Accept(_StringHeader):
+    name = "Accept"
+
+
+class Authorization(_StringHeader):
+    name = "Authorization"
+
+    __slots__ = ("_method",)
+
+    def __init__(self, method, payload):
+        super(Authorization, self).__init__(payload)
+        self.__set_method(method)
+
+    @property
+    def string_value(self):
+        return "{} {}".format(self._method, self._value)
+
+    def __get_method(self):
+        return self._method
+    def __set_method(self, value):
+        self._method = str(value)
+    method = property(__get_method, __set_method, doc="Authentication method")
+
+    def __get_payload(self):
+        return self._value
+    def __set_payload(self, value):
+        self._value = str(value)
+    payload = property(__get_payload, __set_payload,
+            doc="Authentication method payload")
+
+
+class ProxyAuthorization(Authorization):
+    name = "Proxy-Authorization"
+
+
+class WWWAuthenticate(_StringHeader):
+    name = "WWW-Authenticate"
+
+    __slots__ = ("_method",)
+
+    def __init__(self, method, realm):
+        super(WWWAuthenticate, self).__init__(realm)
+        self.__set_method(method)
+
+    @property
+    def string_value(self):
+        return "{} realm={}".format(self._method, self._value)
+
+    def __get_method(self):
+        return self._method
+    def __set_method(self, value):
+        self._method = str(value)
+    method = property(__get_method, __set_method, doc="Authentication method")
+
+    def __get_realm(self):
+        return self._value
+    def __set_realm(self, value):
+        self._value = str(value)
+    realm = property(__get_realm, __set_realm, doc="Authentication realm")
+
+
+class ProxyAuthenticate(WWWAuthenticate):
+    name = "Proxy-Authenticate"
+
+
+class Location(_StringHeader):
+    name = "Location"
---
+++ mohawk/test/test_headers.py
@@ -0,0 +1,123 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Adrian Perez <aperez@igalia.com>
+#
+# Distributed under terms of the MIT license.
+
+import unittest2 as unittest
+from .. import headers as H
+
+header_data = (
+    (H.Accept("text/plain"),
+        "Accept: text/plain\r\n"),
+    (H.Authorization("Basic", "payload"),
+        "Authorization: Basic payload\r\n"),
+    (H.Connection("close"),
+        "Connection: close\r\n"),
+    (H.Connection("keep-alive"),
+        "Connection: keep-alive\r\n"),
+    (H.Connection.CLOSE,
+        "Connection: close\r\n"),
+    (H.Connection.KEEP_ALIVE,
+        "Connection: keep-alive\r\n"),
+    (H.ContentLength(3245),
+        "Content-Length: 3245\r\n"),
+    (H.ContentType("text/plain"),
+        "Content-Type: text/plain\r\n"),
+    (H.Host("some.host.com"),
+        "Host: some.host.com\r\n"),
+    (H.Host("some.host.com:8888"),
+        "Host: some.host.com:8888\r\n"),
+    (H.Host("some.other.host.com", 443),
+        "Host: some.other.host.com:443\r\n"),
+    (H.Location("/"),
+        "Location: /\r\n"),
+    (H.Location("/local/server/path"),
+        "Location: /local/server/path\r\n"),
+    (H.Location("http://other.server.com/path"),
+        "Location: http://other.server.com/path\r\n"),
+    (H.ProxyAuthenticate("Basic", "realm name"),
+        "Proxy-Authenticate: Basic realm=realm name\r\n"),
+    (H.ProxyAuthorization("Basic", "payload"),
+        "Proxy-Authorization: Basic payload\r\n"),
+    (H.Server("python-foo/1.4.2"),
+        "Server: python-foo/1.4.2\r\n"),
+    (H.UserAgent("Mozilla/4.5 (FooBar)"),
+        "User-Agent: Mozilla/4.5 (FooBar)\r\n"),
+    (H.WWWAuthenticate("Basic", "realm name"),
+        "WWW-Authenticate: Basic realm=realm name\r\n"),
+    (H.CustomHeader("X-Powered-By", "Batteries!"),
+        "X-Powered-By: Batteries!\r\n"),
+)
+
+
+class TestHeaderStringRepresentations(unittest.TestCase):
+    @classmethod
+    def setup_from_data(cls, data):
+        for header, representation in data:
+            def f(self):
+                self.assertEquals(representation, str(header))
+            name = "test_" + header.__class__.__name__ + "_repr"
+            count = 0
+            while True:
+                try_name = "{}_{}".format(name, count)
+                count += 1
+                if not hasattr(cls, try_name):
+                    setattr(cls, try_name, f)
+                    break
+
+TestHeaderStringRepresentations.setup_from_data(header_data)
+
+
+class _TestAuthenticateHeaderBase(object):
+    def test_get_set_method(self):
+        self.assertEquals("Basic", self.h.method)
+        self.h.method = "Digest"
+        self.assertEquals("Digest", self.h.method)
+        self.assertEquals(self.h.name + ": Digest realm=foorealm\r\n",
+                str(self.h))
+
+    def test_get_set_realm(self):
+        self.assertEquals("foorealm", self.h.realm)
+        self.h.realm = "barbar"
+        self.assertEquals("barbar", self.h.realm)
+        self.assertEquals(self.h.name + ": Basic realm=barbar\r\n",
+                str(self.h))
+
+
+class TestWWWAuthenticate(unittest.TestCase, _TestAuthenticateHeaderBase):
+    def setUp(self):
+        self.h = H.WWWAuthenticate("Basic", "foorealm")
+
+
+class TestProxyAuthenticate(unittest.TestCase, _TestAuthenticateHeaderBase):
+    def setUp(self):
+        self.h = H.ProxyAuthenticate("Basic", "foorealm")
+
+
+class _TestAuthorizationHeaderBase(object):
+    def test_get_set_method(self):
+        self.assertEquals("Basic", self.h.method)
+        self.h.method = "Digest"
+        self.assertEquals("Digest", self.h.method)
+        self.assertEquals(self.h.name + ": Digest xyzpayload\r\n",
+                str(self.h))
+
+    def test_get_set_payload(self):
+        self.assertEquals("xyzpayload", self.h.payload)
+        self.h.payload = "barbar"
+        self.assertEquals("barbar", self.h.payload)
+        self.assertEquals(self.h.name + ": Basic barbar\r\n",
+                str(self.h))
+
+class TestAuthorization(unittest.TestCase, _TestAuthorizationHeaderBase):
+    def setUp(self):
+        self.h = H.Authorization("Basic", "xyzpayload")
+
+
+class TestProxyAuthorization(unittest.TestCase,
+        _TestAuthorizationHeaderBase):
+    def setUp(self):
+        self.h = H.ProxyAuthorization("Basic", "xyzpayload")
